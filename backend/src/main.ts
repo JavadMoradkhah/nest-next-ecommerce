813ad420492fc1a7c929f859d46a5d44705172e3
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-04-13 19:32:53 +0330

    feat: install cookie parser package to parse cookies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+  app.use(cookieParser(process.env.COOKIE_SECRET));
 
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
