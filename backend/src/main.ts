a79afce3b77c1a97f7aac7faf1bd036ba81d57c7
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-04-13 19:28:03 +0330

    feat: install helmet package to increase server security

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
 bootstrap();
