df24cac4c94baf472ec01adafda30f4c3ec6c5b0
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-04-13 19:50:55 +0330

    feat: serve the "public" directory static content

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,17 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(process.cwd(), 'public'),
     }),
   ],
   controllers: [AppController],
