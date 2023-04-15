35bd99519a50abc4313b14d880630a9935961a8c
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-04-15 20:22:16 +0330

    feat: build the error messages enum module and add category error messages

---
+++ backend/src/enums/error-messages.enum.ts
@@ -0,0 +1,7 @@
+const enum ErrorMessages {
+  CATEGORY_NOT_FOUND = 'The category was not found with the given id',
+  PARENT_CATEGORY_NOT_FOUND = 'The parent category was not found with the given id',
+  CATEGORY_ALREADY_EXISTS = 'The category already exists with the given slug',
+}
+
+export default ErrorMessages;
