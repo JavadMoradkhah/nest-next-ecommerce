ba55f4399e5c9edd03cd5ed3ffaafc805311c0f7
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-04-16 17:04:32 +0330

    refactor: error messages

--- backend/src/enums/error-messages.enum.ts
+++ backend/src/enums/error-messages.enum.ts
@@ -2,7 +2,7 @@ const enum ErrorMessages {
   // Category
   CATEGORY_NOT_FOUND = 'The category was not found with the given id',
   PARENT_CATEGORY_NOT_FOUND = 'The parent category was not found with the given id',
-  CATEGORY_ALREADY_EXISTS = 'The category already exists with the given slug',
+  CATEGORY_ALREADY_EXISTS = 'A category already exists with the given slug',
   // Color
   COLOR_NOT_FOUND = 'The color was not found with the given id',
   COLOR_ALREADY_EXISTS = 'A color already exists with the given name or code',
